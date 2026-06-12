07160b66212cf7580c8581dd57a27e0b5f5123fd
oneslaught <iceminecraft021@gmail.com>
2026-06-12 21:01:24 +0300

    home view added

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
+
+def home_view(request):
+    notes = Note.objects.all().order_by('-created_at')[:6]
+    return render(request, 'notes/home.html', {'notes': notes})
