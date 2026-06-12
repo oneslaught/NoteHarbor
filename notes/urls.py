5b72d5f237f638c391d36180c44a7607ea2b9058
oneslaught <iceminecraft021@gmail.com>
2026-06-12 22:41:00 +0300

    Node detail view added

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('notes/<int:pk>/', views.note_detail_view, name='note_detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Note
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Note
 def home_view(request):
     notes = Note.objects.all().order_by('-created_at')[:6]
     return render(request, 'notes/home.html', {'notes': notes})
+
+def note_detail_view(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/note_detail.html', {'note': note})
