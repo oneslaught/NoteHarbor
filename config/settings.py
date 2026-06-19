5ebc46270fc9bf8fdeb9e32d9ff54bc24122adae
oneslaught <iceminecraft021@gmail.com>
2026-06-19 22:07:34 +0300

    mysql...

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ if os.getenv('USE_SQLITE') == 'True':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
