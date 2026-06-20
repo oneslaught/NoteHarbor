00709427e78f0f1135b1581ffa70895a2e69a9fe
oneslaught <iceminecraft021@gmail.com>
2026-06-20 14:57:32 +0300

    rollback

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if os.getenv('USE_SQLITE') == 'True':
+if os.getenv('USE_SQLITE', 'True') == 'True':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -90,7 +90,7 @@ if os.getenv('USE_SQLITE') == 'True':
 else:
     DATABASES = {
         'default': {
-            'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
